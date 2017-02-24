3a2c7fa4fb645e29c336ef2d4d1b4f78dc2d91d7
Minidodo1 <meier2999@gmail.com>
2017-02-24 20:10:59 +0100

    Update

--- Forge-1.10.2/src/java/me/Minidodo/Scoocyjuice/OurMod/Main.java
+++ Forge-1.10.2/src/java/me/Minidodo/Scoocyjuice/OurMod/Main.java
@@ -10,9 +10,9 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
 
 public class Main {
-	public static final String MODID = "tutorial";
-    public static final String MODNAME = "Tutorial Mod";
-    public static final String VERSION = "1.0.0";
+	public static final String MODID = "MyMod";
+    public static final String MODNAME = "My Mod";
+    public static final String VERSION = "0.0.1";
         
     @Instance
     public static Main instance = new Main();
